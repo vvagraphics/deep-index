e22df3f715fe26952cfaf2858d9893a22e061d13
vvagraphics <vvagraphics@gmail.com>
2026-05-19 23:45:27 -0400

    cs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   nasaApiKey: 'DEMO_KEY',
-  supabaseUrl: 'https://your-project-id.supabase.co',
-  supabaseKey: 'your-anon-public-key-here'           
+  supabaseUrl: '',
+  supabaseKey: ''           
 };
